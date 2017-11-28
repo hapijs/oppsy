8e61426247f668fc7c440bb185583a48e08c5d72
Corbin Uselton <c@decimal.io>
2017-11-27 22:24:02 -0800

    Migrate to node 8 and async/await

--- test/oppsy.js
+++ test/oppsy.js
@@ -8,6 +8,7 @@ const Hapi = require('hapi');
 const Lab = require('lab');
 
 const Network = require('../lib/network');
+const Utils = require('../lib/utils');
 const Oppsy = require('../lib');
 
 
@@ -22,46 +23,44 @@ describe('Oppsy', () => {
 
     describe('constructor()', () => {
 
-        it('is an event EventEmitter', (done) => {
+        it('is an event EventEmitter', () => {
 
             const opps = new Oppsy(new Hapi.Server(), {});
             expect(opps.emit).to.be.a.function();
             expect(opps.on).to.be.a.function();
-            done();
         });
-        it('creates a network monitor and a map of tasks', (done) => {
+        it('creates a network monitor and a map of tasks', () => {
 
             const opps = new Oppsy(new Hapi.Server());
             expect(opps._networkMonitor).to.be.an.instanceof(Network);
-            expect(opps._tasks).to.include(['osload','osmem','osup','psup','psmem','psdelay','requests','concurrents','responseTimes','sockets']);
-            done();
+            expect(opps._tasks).to.include(['osload', 'osmem', 'osup', 'psup', 'psmem', 'psdelay', 'requests', 'responseTimes', 'sockets']);
         });
     });
+
     describe('start()', () => {
 
-        it('emits an "ops" event at the specified interval', (done) => {
+        it('emits an "ops" event at the specified interval', () => {
 
             let count = 0;
             const host = Os.hostname();
             const opps = new Oppsy(new Hapi.Server());
             opps._tasks = {
-                one: (callback) => {
+                one: () => {
 
-                    return callback(null, 'foo');
+                    return 'foo';
                 },
-                two: (callback) => {
+                two: async () => {
 
-                    setTimeout(() => {
+                    await Utils.timeout(40);
 
-                        return callback(null, 'bar');
-                    }, 40);
+                    return 'bar';
                 }
             };
 
             opps.on('ops', (data) => {
 
                 count++;
-                expect(data).to.deep.equal({
+                expect(data).to.equal({
                     one: 'foo',
                     two: 'bar',
                     host
@@ -70,58 +69,80 @@ describe('Oppsy', () => {
                     opps.stop();
                 }
             });
-            opps.on('stop', done);
 
-            opps.start(100);
+            return new Promise((resolve, reject) => {
+
+                opps.on('stop', (err) => {
+
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve();
+                });
+
+                opps.start(100);
+            });
         });
-        it('emits an error if one occurs during processing', (done) => {
+
+        it('emits an error if one occurs during processing', () => {
 
             let count = 0;
             const host = Os.hostname();
             const opps = new Oppsy(new Hapi.Server());
 
             opps._tasks = {
-                one: (callback) => {
+                one: () => {
 
-                    return callback(null, 'foo');
+                    return 'foo';
                 },
-                two: (callback) => {
+                two: () => {
 
-                    if (count % 2 === 0) {
-                        return callback(new Error('there was an error'));
-                    }
-                    callback(null, 'bar');
+                    return new Promise((resolve, reject) => {
+
+                        if (count % 2 === 0) {
+                            reject(new Error('there was an error'));
+                        }
+
+                        return resolve('bar');
+                    });
                 }
             };
 
             opps.on('ops', (data) => {
 
                 count++;
-                expect(data).to.deep.equal({
+                expect(data).to.equal({
                     one: 'foo',
                     two: 'bar',
                     host
                 });
             });
-            opps.on('error', (error) => {
 
-                expect(error).to.be.an.instanceof(Error);
-                expect(error.message).to.equal('there was an error');
-                opps.stop();
-                done();
+            return new Promise((resolve) => {
+
+                opps.on('error', (error) => {
+
+                    expect(error).to.be.an.instanceof(Error);
+                    expect(error.message).to.equal('there was an error');
+                    opps.stop();
+
+                    resolve();
+                });
+
+                opps.start(100);
             });
-
-            opps.start(100);
         });
-        it('does not emit the event after it is stopped', (done) => {
+
+        it('does not emit the event after it is stopped', async () => {
 
             let count = 0;
             const opps = new Oppsy(new Hapi.Server());
 
             opps._tasks = {
-                one: (callback) => {
+                one: () => {
 
-                    return callback(null, 'foo');
+                    return 'foo';
                 }
             };
             opps.on('ops', () => {
@@ -130,19 +151,16 @@ describe('Oppsy', () => {
             });
             opps.start(100);
             opps.stop();
-            setTimeout(() => {
 
-                expect(count).to.equal(0);
-                done();
-            }, 500);
+            await Utils.timeout(500);
+
+            expect(count).to.equal(0);
         });
     });
 
-    it('emits "ops" events with data', (done) => {
+    it('emits "ops" events with data', async () => {
 
         let _data = {};
-        const server = new Hapi.Server();
-        server.connection({ host: 'localhost' });
 
         const opps = new Oppsy(new Hapi.Server());
 
@@ -151,20 +169,22 @@ describe('Oppsy', () => {
             _data = data;
         });
         opps.start(100);
-        setTimeout(() => {
 
-            expect(_data.requests).to.deep.equal({});
-            expect(_data.concurrents).to.deep.equal({});
-            expect(_data.responseTimes).to.deep.equal({});
-            expect(_data.sockets).to.deep.equal({
-                http: { total: 0 },
-                https: { total: 0 }
-            });
-            expect(_data.osload).to.have.length(3);
-            expect(_data.osmem).to.contain('total', 'free');
-            expect(_data).to.contain('osup', 'psup', 'psdelay', 'host');
-            expect(_data.psmem).to.contain('rss', 'heapTotal', 'heapUsed');
-            done();
-        }, 500);
+        await Utils.timeout(500);
+
+        expect(_data.requests).to.equal({});
+        expect(_data.responseTimes).to.equal({});
+        expect(_data.sockets).to.equal({
+            http: {
+                total: 0
+            },
+            https: {
+                total: 0
+            }
+        });
+        expect(_data.osload).to.have.length(3);
+        expect(_data.osmem).to.contain(['total', 'free']);
+        expect(_data).to.contain(['osup', 'psup', 'psdelay', 'host']);
+        expect(_data.psmem).to.contain(['rss', 'heapTotal', 'heapUsed']);
     });
 });
